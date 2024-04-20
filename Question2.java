73c88064201b8f3a19d54d1aeef7531ea5a01384
Kaylea Luk <148406277+chicken88kl@users.noreply.github.com>
2024-04-21 03:39:32 +0800

    did qn 5, removed change from qn 2

--- Question2.java
+++ Question2.java
@@ -11,11 +11,11 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.println("Enter your height in meters: ");
+    System.out.println();
     double height = in.nextDouble();
-    System.out.println("Enter your weight in kilograms: ");
+    System.out.println();
     double weight = in.nextDouble();
     double bmi = weight / (height * height);
-    System.out.println(bmi);
+    System.out.println("Your BMI is " + bmi);
   }
 }
